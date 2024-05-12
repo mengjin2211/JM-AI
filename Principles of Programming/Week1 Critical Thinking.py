e08b8fe3bc9cecca3ff04a991c6fd4d6bf2009ea
Jin Meng <mengjin2211@gmail.com>
2024-05-11 17:23:27 -0700

    adding and substracting

---
+++ Principles of Programming/Week1 Critical Thinking.py
@@ -0,0 +1,10 @@
+"""Part 1:
+Write a Python program to find the addition and subtraction of two numbers.
+Ask the user to input two numbers (num1 and num2). Given those two numbers, add them together to find the output. 
+Also, subtract the two numbers to find the output.
+"""
+
+num1=input()
+numb2=input()
+print(num1 + "+" + num2 + "=" + str(num1 + num2))
+print(num1 + "-" + num2 + "=" + str(num1 - num2))
