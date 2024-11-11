8a657db1733ca4012558a3ce46c6b94a6c3dd565
Jin Meng <mengjin2211@gmail.com>
2024-11-11 12:24:29 -0800

    revised

--- Algorism/Module1 Critical Thinking -Linear Search Online Marketplace Systems.py
+++ Algorism/Module1 Critical Thinking -Linear Search Online Marketplace Systems.py
@@ -1,10 +1,40 @@
-#Linear Search Online Marketplace Systems 
-#In real-world scenario, I would first build connection with the sql database using APIs and query the items to build items list. 
+"""Linear Search Online Marketplace Systems 
+    1) Simulate connection to postgres database using retrieve_data(), existing with a manually created items list.
+    2) define_criteria() gives user flexibility to choose customized keywords to search the marketplace. If no keyword is provided, then default None.
+    3) linear_search() searches through all items in the marketplace for perfect match or partial match, appending results to two arrays.
+    4) print_items() prints the search result.
+"""
 
-items = [
-    {'id': 1, 'item_name': 'hand warmer', 'price': 28.5, 'category': 'electronics', 'description': 'keep your hands warm in winter'},
-    {'id': 2, 'item_name': 'long johns', 'price': 15, 'category': 'clothing', 'description': 'cotton'},
-    {'id': 3, 'item_name': 'chair', 'price': 60, 'category': 'furniture', 'description': 'good quality'},]
+#import psycopg2
+def retrieve_data():
+    conn_params = None
+    try:
+        connection = psycopg2.connect(**conn_params)
+        cursor = connection.cursor()
+        cursor.execute("SELECT item_name, price, category, description FROM marketplace_items")
+        item_rows = cursor.fetchall()
+        items = [
+            {   'item_name': row[0],
+                'price': row[1],
+                'category': row[2],
+                'description': row[3]
+            }
+            for row in item_rows
+        ]       
+        return items   
+    except Exception as e:
+        print("Error connecting to the database. Create your own data instead")
+        items = [
+        {'item_name': 'hand warmer', 'price': 28.5, 'category': 'electronics', 'description': 'keep your hands warm in winter'},
+        {'item_name': 'long johns', 'price': 15, 'category': 'clothing', 'description': 'cotton'},
+        {'item_name': 'chair', 'price': 60, 'category': 'furniture', 'description': 'good quality'},]
+        return items
+    finally:
+        try:
+            cursor.close()
+            connection.close()
+        except:
+            pass
 
 def search_criteria(item=None, Price=None, Category=None, Desc=None):   
     search={'item_name': item, 'price': Price, 'category': Category, 'description':Desc}
@@ -26,10 +56,12 @@ def linear_search (items, search):
         elif 0<match_count < len(searchKey):
             partial_match.append(item)             
     return match, partial_match
+
 def print_items(search, match, partial_match):
-    print(f'for search criteria {search}','\n'
+    print(f'for search criteria {search.values()}','\n'
         f'{len(match)} perfect match: {match}\n{len(partial_match)} partial match:{partial_match}')
 
+items=retrieve_data()
 search1 = search_criteria(item='hand warmer', Category='electronics')
 match, partial_match = linear_search(items, search1)
 print_items(search1,match, partial_match)
