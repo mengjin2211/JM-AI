fd744b4bae67d1e1924e3f69453e8419053cf5a2
mengjin2211 <mengjin2211@gmail.com>
2025-02-09 19:39:35 -0800

    Optimization

--- Operating Systems/Week6 Critical Thinking.py
+++ Operating Systems/Week6 Critical Thinking.py
@@ -1,14 +1,15 @@
 import random
 import time
-import os 
+import os
+import numpy as np
 import multiprocessing 
-def RandomNumbers(file, start, end):
-    #seq_time_start = time.time()
+
+def optimized_random_numbers(file, start, end, chunk=100000):
+    """ Optimization with Numpy and Chunks."""
     with open(file, "w") as f:
-        for _ in range(start, end):
-            f.write(f"{random.randint(0, 32767)}\n")
-    #seq_time_end = time.time()
-    #print(f'Sequential Streaming Processing Time: {seq_time_end - seq_time_start:.4f} seconds.')
+        for _ in range((end - start) // chunk):
+            numbers = np.random.randint(0, 32767, size=chunk, dtype=np.int32)
+            f.write("\n".join(map(str, numbers)) + "\n")
 
 def removefile (file):
     os.remove(file) if os.path.exists(file) else print(f'{file} does not exist.')
@@ -29,7 +30,7 @@ def parallel_processing(num_files, total_rows=10000000):
         start = i * chunk_size
         end = (i + 1) * chunk_size if i != num_files - 1 else total_rows
         
-        p = multiprocessing.Process(target=RandomNumbers, args=(file_name, start, end))
+        p = multiprocessing.Process(target=optimized_random_numbers, args=(file_name, start, end))
         processes.append(p)
         p.start()
     
@@ -40,10 +41,8 @@ def parallel_processing(num_files, total_rows=10000000):
     print(f"Parallel Processing Time ({num_files} files): {end_time - start_time:.4f} seconds")
     remove_files(num_files)
 
-
 if __name__ == "__main__":
     removefile ("file1.txt")
-    RandomNumbers("file1.txt", 0, 10000000)
+    optimized_random_numbers("file1.txt", 0, 10000000)
     for num_files in [1, 2, 5, 10, 20]:
-        parallel_processing(num_files=num_files) 
- 
+        parallel_processing(num_files=num_files) 	
