35e8988a0090f18617ad62c4abfc412070377d9e
Jin Meng <mengjin2211@gmail.com>
2025-02-16 20:00:32 -0800

    update

--- Operating Systems/parallel_processing.py
+++ Operating Systems/parallel_processing.py
@@ -3,10 +3,6 @@ import time
 import sys
 
 def process_files(file1, file2, output):
-    """
-    Reads the entire content of file1 and file2 (which are chunks),
-    adds corresponding lines, and writes the result to the output file.
-    """
     with open(file1, 'r') as f1, open(file2, 'r') as f2, open(output, 'w') as out:
         for line1, line2 in zip(f1, f2):
             try:
@@ -19,8 +15,7 @@ if __name__ == '__main__':
     if len(sys.argv) != 4:
         print("Usage: python script.py <file1> <file2> <output>")
         sys.exit(1)
- 
-        process_files(sys.argv[1], sys.argv[2], sys.argv[3])
+    process_files(sys.argv[1], sys.argv[2], sys.argv[3])
 
         
  
