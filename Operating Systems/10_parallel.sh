a3be8fa3e9a10d880e4b1742e5fc25c57cf283e1
mengjin2211 <108208045+mengjin2211@users.noreply.github.com>
2025-02-17 10:51:03 -0800

    Update 10_parallel.sh

--- Operating Systems/10_parallel.sh
+++ Operating Systems/10_parallel.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+set -x
+set -e
+set -u
 
 start_time=$SECONDS
 
 
 for i in {1..10}; do
-	python3 parallel_processing.py hugefile1_10_$i.txt hugefile2_10_$i.txt totalfile_10_$i.txt &
+	num=$(printf "%02d" $i)
+	python3 parallel_processing.py hugefile1_10_"$num".txt hugefile2_10_"$num".txt totalfile_10_"$num".txt &
 done
 
 wait 
 
-cat totalfile_10_*.txt>totalfileB.txt
+cat totalfile_10_*.txt > totalfileB.txt
+
 end_time=$SECONDS
 diff=$((end_time-start_time))
 
-echo "Processing 10 parallel takes: $diff seconds."
+echo "Processing 10 parallel takes: $diff seconds.
+
